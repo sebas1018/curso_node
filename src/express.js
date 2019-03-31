1089689035661c06cc071d62e8dd3ef66d9fcdb1
Sebastian Rojas Quintero <srojas@sumset.com>
2019-03-31 12:13:51 -0500

    helpers

--- src/express.js
+++ src/express.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const hbs = require('hbs');
+require('./helpers')
 
 const directorioPublico = path.join(__dirname, '../public');
 const directorioPartials = path.join(__dirname, '../partials');
@@ -12,8 +13,14 @@ app.set("view engine", "hbs");
 
 app.get('/',(req, res) => {
   res.render('index',{
-  	title: 'Pagina de prueba'
+    title: 'Pagina de prueba',
+    estudiante : 'Sebastian',
+    nota1 : 4,
+    nota2 : 3,
+    nota3 : 5
   });
 });
  
-app.listen(3000);
+app.listen(3000,() => {
+  console.log("corriendo en 3000");
+});
