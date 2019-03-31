c9bcfb76eb15b396a0e6f72a586bc5a841baaed6
Sebastian Rojas Quintero <srojas@sumset.com>
2019-03-31 14:01:19 -0500

    Manejo envío datos por post y uso de formularios

--- src/express.js
+++ src/express.js
@@ -2,23 +2,32 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const hbs = require('hbs');
+const bodyParser = require('body-parser');
 require('./helpers')
 
 const directorioPublico = path.join(__dirname, '../public');
 const directorioPartials = path.join(__dirname, '../partials');
 app.use(express.static(directorioPublico));
 hbs.registerPartials(directorioPartials);
-
+app.use(bodyParser.urlencoded({extended:false}));
 app.set("view engine", "hbs");
 
-app.get('/',(req, res) => {
-  res.render('index',{
+app.get('/',(req,res) =>{
+  res.render('index');
+});
+
+app.post('/calculos',(req, res) => {
+  res.render('calculos',{
     title: 'Pagina de prueba',
-    estudiante : req.query.nombre,
-    nota1 : parseInt(req.query.nota1),
-    nota2 : parseInt(req.query.nota2),
-    nota3 : parseInt(req.query.nota3)
+    estudiante : req.body.nombre,
+    nota1 : parseInt(req.body.nota1),
+    nota2 : parseInt(req.body.nota2),
+    nota3 : parseInt(req.body.nota3)
   });
+});
+
+app.get('*',(req, res) =>{
+  res.render('error.hbs');
 });
  
 app.listen(3000,() => {
